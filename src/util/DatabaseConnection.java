85189833f0277baa4960fc241bf90bc7b3fa2991
yederlv <yedjovic17@gmail.com>
2024-05-16 19:32:23 -0500

    Refactor: add proyecto sistema veterinaria

---
+++ src/util/DatabaseConnection.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Yeder LV
+ */
+public class DatabaseConnection {
+    
+    private static final String URL = "jdbc:mysql://localhost:3306/veterinaria";
+    private static final String USER = "root";
+    private static final String PASSWORD = "password";
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                System.out.println("Conexión exitosa a la base de datos.");
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("Error al conectar a la base de datos.");
+            }
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+}
